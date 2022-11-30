983347d0eba26d2f5ab2c42e1c4912e027e2e6b3
Marius <marius@example.com>
2022-11-30 14:13:42 +0200

    Container

--- src/components/gainMore/GainMore.jsx
+++ src/components/gainMore/GainMore.jsx
@@ -1,6 +1,11 @@
+import Container from '../UI/container/Container';
 import Section from './../UI/section/Section';
 
 function GainMore(props) {
-  return <Section>GainMore</Section>;
+  return (
+    <Section>
+      <Container>Gain more section title</Container>
+    </Section>
+  );
 }
 export default GainMore;
