6eaf3488457815fa4434e9f71e7ca53ea0423d1c
Marius <marius@example.com>
2022-11-30 15:04:23 +0200

    generuoti SingleCard komponetus

--- src/components/GainCardsList.jsx
+++ src/components/GainCardsList.jsx
@@ -4,6 +4,7 @@ function GainCardsList(props) {
   return (
     <Grid>
       {props.items.map((cObj) => (
+        // generuoti SingleCard komponetus
         <li key={cObj.id}>{cObj.title}</li>
       ))}
     </Grid>
