637e536e11b6073b3813585dc6c7ca03b82ad07d
Marius <marius@example.com>
2022-12-01 08:30:14 +0200

    icons list ok

--- src/components/singleCard/SingleCard.jsx
+++ src/components/singleCard/SingleCard.jsx
@@ -4,8 +4,8 @@ import css from './SingleCard.module.css';
 function SingleCard(props) {
   return (
     <li className={css.grid}>
-      <Icon>{props.icon}</Icon>
-      <h3 className='title'>{props.title}</h3>
+      <Icon className={css.icon}>{props.icon}</Icon>
+      <h3 className={css.title}>{props.title}</h3>
       <p className='text'>{props.text}</p>
     </li>
   );
